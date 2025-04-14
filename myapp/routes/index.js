88e7c0bd53c15e6b51e13df1d04d11c474c450f8
Ema Fresco <efresco@udesa.edu.ar>
2025-04-14 17:36:12 -0300

    include partials header y headerlogueado

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,7 +5,4 @@ const productController = require('../controllers/productController');
 
 /* GET home page. */
 router.get('/', indexController.index);
-router.get('/register', indexController.register);
-router.get('/login', indexController.login);
-router.get('/logout', indexController.logout);
 module.exports = router;
