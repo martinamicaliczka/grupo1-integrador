a05973719de7eac5e4b7f425e3e318f29b2e2f95
Lola Otzet <lotzet@udesa.edu.ar>
2025-04-14 15:24:26 -0300

    pagina de detalle

--- myapp/controllers/productController.js
+++ myapp/controllers/productController.js
@@ -1,12 +1,19 @@
 const data = require('../db/db'); 
 const usuarios = data.usuarios;
 const productos = data.productos;
+const comentarios = data.comentarios;
+
 const productController = {
    product: function (req, res) {
-      return res.render('product')
+      const nombreProducto = productos[0].nombre;
+		const descripcionProducto = productos[0].descripcion;
+		const imgProducto = productos[0].imagenProducto;
+      const imgUsuario = usuarios.fotoPerfil;
+      const nombreUsuario = usuarios.usuario;
+      return res.render('product', {productos:productos, nombreProducto:nombreProducto, descripcionProducto:descripcionProducto,imgProducto:imgProducto, comentarios:comentarios, imgUsuario: imgUsuario, nombreUsuario:nombreUsuario})
    },
    homeProduct: function(req,res){
-      return res.render('product', {productos: data.productos})
+      return res.render('product')
    },
     results: function (req, res) {
       const nombreProducto = productos[0].nombre;
